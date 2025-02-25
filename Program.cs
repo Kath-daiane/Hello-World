736db253dc2b54ce29186f3bdcf41fd07cf9549b
Kath-daiane <kath.daiane.p@gmail.com>
2025-02-25 11:27:48 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+    class Program
+{
+    static void Main ()
+    {
+        Console.WriteLine("Hallo World!");
+        Console.WriteLine("Programar em C# é muito legal!");
+    }
+}
